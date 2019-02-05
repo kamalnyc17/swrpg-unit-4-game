baf21b27182a80ad8b7ca1d10bc1e5778a0c0b04
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-02-05 18:17:52 -0500

    Star Wars RPG Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 $(document).ready( function() {
-
-    $('.your-charecter1>h1').appendTo( $('.enemy-fighter1') );
-    $(".enemy-fighter1").css({"background-color": "red", "padding": "20px 20px", "height": "100px", "width": "100px", "float": "left"});
+    $('.your-charecter1>#char1').appendTo( $('.enemy-fighter1') );
+    $('.your-charecter1>img').appendTo( $('.enemy-fighter1') );
+    $('.your-charecter1>#point1').appendTo( $('.enemy-fighter1') );
+    //$(".enemy-fighter1").css({"background-color": "red", "padding": "20px 20px", "height": "100px", "width": "100px", "float": "left"});
     $(".your-charecter4").hide();
 })
 
